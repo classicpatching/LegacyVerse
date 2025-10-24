d1fe70efb80e1ba71a089f52057d38d34903a756
classicpatching <2014.ct.ws@gmail.com>
2025-10-24 16:31:07 +0500

    Create index.php

---
+++ mobileapi/securesignup/index.php
@@ -0,0 +1,12 @@
+{
+  "Status": "OK",
+  "UserInfo": {
+    "UserName": "sign",
+    "UserPassword": "acc",
+    "UserID": 2,
+    "RobuxBalance": 0,
+    "TicketsBalance": 0,
+    "IsAnyBuildersClubMember": true,
+    "ThumbnailUrl": "https://14blox.strangled.net/getUserAvatarImage?userId=1"
+  }
+}
