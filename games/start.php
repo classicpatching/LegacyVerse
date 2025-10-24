4bdcae31ac75cefedaed8d9856ef2994fa7194e3
classicpatching <2014.ct.ws@gmail.com>
2025-10-24 21:42:57 +0500

    Update start.php

--- games/start.php
+++ games/start.php
@@ -6,7 +6,7 @@ $placeId = $_GET['placeId'] ?? 1;
 $userId = $_GET['userId'] ?? 1;
 
 // This is your joinScriptUrl — points to the same start.php
-$joinUrl = "http://www.fotone.com/games/start.php?placeId={$placeId}&userId={$userId}";
+$joinUrl = "http://www.fotone.com/games/start?placeId={$placeId}&userId={$userId}";
 
 $response = [
     "status" => "2",
