ef266a2085d05588462ffc392b258f34aad48da5
classicpatching <2014.ct.ws@gmail.com>
2025-10-24 16:32:12 +0500

    Update index.php

--- mobileapi/securesignup/index.php
+++ mobileapi/securesignup/index.php
@@ -1,12 +1,19 @@
-{
-  "Status": "OK",
-  "UserInfo": {
-    "UserName": "sign",
-    "UserPassword": "acc",
-    "UserID": 2,
-    "RobuxBalance": 0,
-    "TicketsBalance": 0,
-    "IsAnyBuildersClubMember": true,
-    "ThumbnailUrl": "https://14blox.strangled.net/getUserAvatarImage?userId=1"
-  }
-}
+<?php
+header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Origin: *"); // allow APK or browser requests
+
+$response = [
+    "Status" => "OK",
+    "UserInfo" => [
+        "UserName" => "sign",
+        "UserPassword" => "acc",
+        "UserID" => 2,
+        "RobuxBalance" => 0,
+        "TicketsBalance" => 0,
+        "IsAnyBuildersClubMember" => true,
+        "ThumbnailUrl" => "https://14blox.strangled.net/getUserAvatarImage?userId=1"
+    ]
+];
+
+echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+exit;
