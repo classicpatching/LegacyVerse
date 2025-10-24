dfb373bc59b248b19d9b55459dbf6a40f4dac51b
classicpatching <2014.ct.ws@gmail.com>
2025-10-25 01:27:30 +0500

    Update index.php

--- mobileapi/login/index.php
+++ mobileapi/login/index.php
@@ -1,18 +1,30 @@
 <?php
-header("Content-Type: application/json; charset=utf-8");
-header("Access-Control-Allow-Origin: *");
-
-$response = [
-    "Status" => "OK",
-    "UserInfo" => [
-        "UserName" => "qa",
+header('Content-Type: application/json; charset=UTF-8; X-Robots-Tag: noindex');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if ($_POST["password"] == "pass1234" && $_POST["username"] == "test") {
+  echo(json_encode([
+      "Status" => "OK",
+      "UserInfo" => [
         "UserID" => 1,
-        "RobuxBalance" => 10,
-        "TicketsBalance" => 100,
+        "UserName" => $_POST["username"],
+        "RobuxBalance" => 0,
+        "TicketsBalance" => 0,
         "IsAnyBuildersClubMember" => false,
-        "ThumbnailUrl" => "https://legacyverse.onrender.com/avatar.webp"
-    ]
-];
-
-echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-exit;
+        "ThumbnailUrl" => "http://yourthumbnail.here/or_this_can_be_a_blank"
+      ]
+  ],JSON_UNESCAPED_SLASHES));
+  } else {
+    echo(json_encode([
+      "Status" => "InvalidPassword",
+      "UserInfo" => [
+        "UserID" => 1,
+        "UserName" => $_POST["username"],
+        "RobuxBalance" => 0,
+        "TicketsBalance" => 0,
+        "IsAnyBuildersClubMember" => false,
+        "ThumbnailUrl" => "http://yourthumbnail.here/or_this_can_be_a_blank"
+      ]
+  ],JSON_UNESCAPED_SLASHES));
+  }
+}
+?>
