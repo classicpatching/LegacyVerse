f5dd47238da7428a1be69534314e38f0cef93715
classicpatching <2014.ct.ws@gmail.com>
2025-10-24 14:07:05 +0500

    Update list.php

--- games/list.php
+++ games/list.php
@@ -6,9 +6,9 @@
 </head>
 <body>
     <h1>Games List</h1>
-    <form action="start.php" method="get">
-        <input type="hidden" name="placeid" value="1">
-        <input type="hidden" name="userid" value="1">
+    <form action="start" method="get">
+        <input type="hidden" name="placeId" value="1">
+        <input type="hidden" name="userId" value="1">
         <button type="submit">Play</button>
     </form>
 </body>
