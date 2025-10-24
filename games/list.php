bf552bc40befbf352bc64fb81bdd3d6b54c566f7
classicpatching <2014.ct.ws@gmail.com>
2025-10-24 13:57:59 +0500

    Create list.php

---
+++ games/list.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Games List</title>
+</head>
+<body>
+    <h1>Games List</h1>
+    <form action="start.php" method="get">
+        <input type="hidden" name="placeid" value="1">
+        <input type="hidden" name="userid" value="1">
+        <button type="submit">Play</button>
+    </form>
+</body>
+</html>
