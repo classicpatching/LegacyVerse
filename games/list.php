8269c7fd88979d6fab6f2cb6a80d33c444642d34
classicpatching <2014.ct.ws@gmail.com>
2025-10-24 21:42:09 +0500

    Update list.php

--- games/list.php
+++ games/list.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Games List</title>
-</head>
-<body>
-    <h1>Games List</h1>
-    <form action="start" method="get">
-        <input type="hidden" name="placeid" value="1">
-        <input type="hidden" name="userId" value="1">
-        <button type="submit">Play</button>
-    </form>
-</body>
-</html>
+<?php
+// list.php
+$games = [
+    ["placeId" => 1, "name" => "Test Game", "userId" => 1],
+    ["placeId" => 2, "name" => "Another Game", "userId" => 2]
+];
+?>
+<h1>Available Games</h1>
+<ul>
+<?php foreach($games as $game): ?>
+    <li>
+        <a href="start.php?placeId=<?= $game['placeId'] ?>&userId=<?= $game['userId'] ?>">
+            <?= $game['name'] ?>
+        </a>
+    </li>
+<?php endforeach; ?>
+</ul>
