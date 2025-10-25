945d0191d413862541912a372fb4e64c3ea9d961
classicpatching <2014.ct.ws@gmail.com>
2025-10-25 21:50:08 +0500

    Update index.php

--- mobileapi/userinfo/index.php
+++ mobileapi/userinfo/index.php
@@ -1,11 +1,15 @@
-{
-  "UserInfo": {
-    "UserName": "testuser",
-    "UserPassword": "hello",
-    "UserID": 1,
-    "RobuxBalance": 150,
-    "TicketsBalance": 12000,
-    "IsAnyBuildersClubMember": true,
-    "ThumbnailUrl": "https://14blox.strangled.net/getUserAvatarImage?userId=1"
-  }
-}
+<?php
+header('Content-Type: application/json');
+
+echo json_encode([
+    "UserInfo" => [
+        "UserName" => "testuser",
+        "UserPassword" => "hello",
+        "UserID" => 1,
+        "RobuxBalance" => 150,
+        "TicketsBalance" => 12000,
+        "IsAnyBuildersClubMember" => true,
+        "ThumbnailUrl" => "https://14blox.strangled.net/getUserAvatarImage?userId=1"
+    ]
+]);
+?>
