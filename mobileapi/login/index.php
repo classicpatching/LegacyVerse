1f071dcdc8452355215354ecf0dcbf412b283716
classicpatching <2014.ct.ws@gmail.com>
2025-10-25 09:29:57 +0500

    Update index.php

--- mobileapi/login/index.php
+++ mobileapi/login/index.php
@@ -1,16 +1,16 @@
 <?php
 header('Content-Type: application/json; charset=UTF-8; X-Robots-Tag: noindex');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if ($_POST["password"] == "pass1234" && $_POST["username"] == "test") {
+  if ($_POST["password"] == "12345678" && $_POST["username"] == "Player") {
   echo(json_encode([
       "Status" => "OK",
       "UserInfo" => [
         "UserID" => 1,
         "UserName" => $_POST["username"],
-        "RobuxBalance" => 0,
-        "TicketsBalance" => 0,
+        "RobuxBalance" => 10,
+        "TicketsBalance" => 100,
         "IsAnyBuildersClubMember" => false,
-        "ThumbnailUrl" => "http://yourthumbnail.here/or_this_can_be_a_blank"
+        "ThumbnailUrl" => "http://legacyverse.onrender.com/avatar.webp"
       ]
   ],JSON_UNESCAPED_SLASHES));
   } else {
@@ -19,10 +19,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       "UserInfo" => [
         "UserID" => 1,
         "UserName" => $_POST["username"],
-        "RobuxBalance" => 0,
-        "TicketsBalance" => 0,
+        "RobuxBalance" => 10,
+        "TicketsBalance" => 100,
         "IsAnyBuildersClubMember" => false,
-        "ThumbnailUrl" => "http://yourthumbnail.here/or_this_can_be_a_blank"
+        "ThumbnailUrl" => "http://legacyverse.onrender.com/avatar.webp"
       ]
   ],JSON_UNESCAPED_SLASHES));
   }
