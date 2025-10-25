9861fa2b11fd31226e9313a18bcbff5ef138752a
classicpatching <2014.ct.ws@gmail.com>
2025-10-25 21:49:08 +0500

    Create index.php

---
+++ mobileapi/userinfo/index.php
@@ -0,0 +1,11 @@
+{
+  "UserInfo": {
+    "UserName": "testuser",
+    "UserPassword": "hello",
+    "UserID": 1,
+    "RobuxBalance": 150,
+    "TicketsBalance": 12000,
+    "IsAnyBuildersClubMember": true,
+    "ThumbnailUrl": "https://14blox.strangled.net/getUserAvatarImage?userId=1"
+  }
+}
