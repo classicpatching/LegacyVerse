d320ec577ea9fc4f4dda8171fc485ce5761b85b4
classicpatching <2014.ct.ws@gmail.com>
2025-10-25 22:11:19 +0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,9 +1,7 @@
 <?php
 header("Content-Type: text/plain; charset=utf-8");
-http_response_code(404); // Proper HTTP 404
+http_response_code(404);
 
-// Get the requested path
-$requested_path = $_SERVER['REQUEST_URI'];
-
-echo "Error: The path '$requested_path' does not exist.";
+$path = $_SERVER['REQUEST_URI'];
+echo "Error: The path '$path' does not exist.";
 exit;
