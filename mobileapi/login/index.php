999fcefecf3039c93efab7425c632af3699450f7
classicpatching <2014.ct.ws@gmail.com>
2025-10-25 21:47:36 +0500

    Update index.php

--- mobileapi/login/index.php
+++ mobileapi/login/index.php
@@ -1,30 +1,15 @@
 <?php
-header('Content-Type: application/json; charset=UTF-8; X-Robots-Tag: noindex');
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if ($_POST["password"] == "0987654321123" && $_POST["username"] == "roblox") {
-  echo(json_encode([
-      "Status" => "OK",
-      "UserInfo" => [
+header('Content-Type: application/json');
+
+echo json_encode([
+    "UserInfo" => [
+        "UserName" => "testuser",
+        "UserPassword" => "hello",
         "UserID" => 1,
-        "UserName" => $_POST["username"],
-        "RobuxBalance" => 1000000,
-        "TicketsBalance" => 1000000,
-        "IsAnyBuildersClubMember" => false,
-        "ThumbnailUrl" => "https://legacyverse.onrender.com/1.webp"
-      ]
-  ],JSON_UNESCAPED_SLASHES));
-  } else {
-    echo(json_encode([
-      "Status" => "InvalidPassword",
-      "UserInfo" => [
-        "UserID" => 1,
-        "UserName" => $_POST["username"],
-        "RobuxBalance" => 1000000,
-        "TicketsBalance" => 1000000,
-        "IsAnyBuildersClubMember" => false,
-        "ThumbnailUrl" => "https://legacyverse.onrender.com/1.webp"
-      ]
-  ],JSON_UNESCAPED_SLASHES));
-  }
-}
+        "RobuxBalance" => 150,
+        "TicketsBalance" => 12000,
+        "IsAnyBuildersClubMember" => true,
+        "ThumbnailUrl" => "https://14blox.strangled.net/getUserAvatarImage?userId=1"
+    ]
+]);
 ?>
